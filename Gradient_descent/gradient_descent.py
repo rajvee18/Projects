aa19d91fe0c0f0d03d5fe696a41774341a4641f7
rajvee18 <rajvee23@iisertvm.ac.in>
2024-09-16 13:32:58 +0530

    gradient_descent

--- Gradient_descent/gradient_descent.py
+++ Gradient_descent/gradient_descent.py
@@ -1,26 +1,36 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def gradient_descent(x,y):
     m_curr = b_curr = 0
-    iterations = 100
+    iterations = 1000
     n = len(x)
-    learning_rate = 0.001 
+    learning_rate = 0.045
+    
 
     for i in range(iterations):
+
         y_predicted = m_curr*x + b_curr
+        
         cost = (1/n)*sum([val**2 for val in (y-y_predicted)])
         md = -(2/n)*sum(x*(y-y_predicted))
         bd = -(2/n)*sum(y-y_predicted)
-
         m_curr =m_curr - learning_rate * md
         b_curr = b_curr - learning_rate * bd
 
+        
+
         print(f"m {m_curr}, b {b_curr}, iteration {i}, cost {cost}")
+    
 
 x = np.array([1,2,3,4,5])
 y = np.array([100,567,987,464,124])
 
 gradient_descent(x,y)
+
+
+
+
 
 
         
